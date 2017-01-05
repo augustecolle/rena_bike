01a926d5b3ac1750661fa615ed3c843983f5afa6
rienleenders <Rien@MacBook-Air-van-EA.local>
2017-01-05 19:21:33 +0100

    Start

---
+++ test.py
@@ -0,0 +1,26 @@
+import googlemaps
+import pprint as pp
+import pandas as pd
+import matplotlib.pyplot as plt
+
+samples = 200
+startPoint = "burgstraat gent"
+endPoint = "oudleikestraat ruiselede"
+
+gmaps = googlemaps.Client(key="AIzaSyAWUyZr-YD30e0gDhqrJqML9VriCurSsJA")
+#Get directions and get the polyline
+directionResult = gmaps.directions(startPoint, endPoint)
+encLine = str(directionResult[0]["overview_polyline"]["points"])
+#Get elevation points
+elevationResult = gmaps.elevation_along_path(path=encLine, samples=samples)
+#Push the result into a dataframe
+data = pd.DataFrame(columns=["resolution","elevation", "lat", "lng"])
+for point in elevationResult:	
+	temp = pd.DataFrame({"resolution": [point["resolution"]], "elevation": [point["elevation"]], "lat":[point["location"]["lat"]], "lng":[point["location"]["lng"]]})
+	data = data.append(temp, ignore_index=True)	
+
+print data
+
+fig = plt.figure()
+plt.plot(data["elevation"])
+plt.show()
