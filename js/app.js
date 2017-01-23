6e17e7c72772afd2dc61784eae0a1ca6e86317eb
Auguste <augustecolle@hotmail.com>
2017-01-23 10:14:37 +0000

    initial commit rasp

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function(){
+  var app = angular.module("renaBike", ["ngRoute"]);
+  
+  app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix(''); //http://stackoverflow.com/questions/41214312/exclamation-mark-after-hash-in-angularjs-app/41551864#41551864
+    $routeProvider
+    .when("/", {templateUrl: "map.html", controller: "mapCtrl"})
+    .when("/statistics", {templateUrl: "statistics.html", controller: "statCtrl"});
+    //.when("/dashboard", {templateUrl: "statistics.html", controller: "statCtrl"});
+    //.when("/weather", {templateUrl: "statistics.html", controller: "statCtrl"});
+    //.otherwise({redirectTo: "/map"});    
+  });
+  
+  app.controller("mapCtrl", function($scope){
+    closeNav();
+    autoResizeDiv();
+    mapbox();
+  });
+  
+  app.controller("statCtrl", function($scope){
+    closeNav();
+    
+  });
+  
+})();
---
+++ js/mapbox.js
@@ -0,0 +1,91 @@
+//GLOBAL VARIABLES
+var rider;
+var map;
+
+function mapbox(){
+	mapboxgl.accessToken = 'pk.eyJ1IjoiYXVndXN0ZWNvbGxlIiwiYSI6ImNpeHE5b2p3YjAwMjgzM3AxYW11YTdqcm8ifQ.rWupKvdQ1UV6q4xJCBGKUw';
+	
+	map = new mapboxgl.Map({
+	  container: 'map',
+	  style: 'mapbox://styles/mapbox/outdoors-v9',
+	  zoom: 18,
+	  center: [3.7174, 51.0543]
+	});
+		
+	var directions = new MapboxDirections({
+		accessToken: mapboxgl.accessToken,
+	  unit: 'metric', // Use the metric system to display distances.
+		controls: {
+			instructions: false,
+		},
+	  profile: 'cycling', // Set the initial profile to walking.
+	  container: 'directions', // Specify an element thats not the map container.
+	  proximity: [3.7174, 51.0543] // Give search results closer to these coordinates higher priority.
+	});
+	
+	function labelCurrentPosition(lat, lng){
+		console.log("labelCurrentPosition");
+		var el = document.createElement('div');
+		//el.style.backgroundColor='red';
+		el.style.backgroundImage = "url('images/rider.svg')";
+		el.className = 'marker';
+		el.id = 'rider';
+		el.style.width = '45' + 'px';
+		el.style.height = '45' + 'px';
+		var marker = new mapboxgl.Marker(el);
+		marker.setLngLat([lng, lat]);
+		marker.addTo(map);
+		return marker;
+	}
+	
+	var positionIndicator;
+	map.on('load', function () {
+		var nav = new mapboxgl.NavigationControl();
+		map.addControl(nav, 'bottom-left');
+		console.log("onload function")
+		//set center on current location with HTML5
+		var pos;
+	
+		if (navigator.geolocation) {
+		  navigator.geolocation.getCurrentPosition(function(position) {
+				console.log("getting position")
+		  	pos = {
+		      lat: position.coords.latitude,
+		      lng: position.coords.longitude
+		    };
+			rider = labelCurrentPosition(position.coords.latitude, position.coords.longitude);
+			map.setCenter([pos.lng, pos.lat]);
+		  });
+			postionIndicator = navigator.geolocation.watchPosition(function(position) {
+				console.log("positionIndicator")
+				rider.setLngLat([position.coords.longitude, position.coords.latitude]);
+				console.log(position.coords.latitude, position.coords.longitude);
+			});
+		}
+		map.addControl(directions);
+		map.addControl(new mapboxgl.GeolocateControl({
+		  positionOptions: {
+		    enableHighAccuracy: true,
+				watchPosition: true
+		  }
+		}), 'bottom-right');
+	});
+
+	directions.on('route', function(e) {
+		console.log(e.route); // Logs the current route shown in the interface.
+	});
+
+	//change rider figure to zoom level
+	map.on('zoomend', function() {
+		var currentZoom = map.getZoom();
+		var size = 45*currentZoom/18 + "px"
+		rider._element.style.backgroundSize = size + " " + size;
+		rider._element.style.width = size;
+		rider._element.style.height = size;
+	});
+
+//  To edit the input boxes
+//	document.getElementById("mapbox-directions-origin-input").children[0].children[1].value="TEST"
+//	mapbox-directions-destination-input
+//	mapbox-directions-origin-input
+}	
