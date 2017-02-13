2c552aab8fa4455df97db197adc5ac7e5280b5b8
Auguste Colle <auguste.colle@hotmail.com>
2017-02-13 09:37:29 +0100

    finetuning

--- flask/app.py
+++ flask/app.py
@@ -161,7 +161,7 @@ class Energy(Resource):
         self.tr.get_distances()
         self.tr.get_compass_bearing()
         self.tr.get_slopes()
-        self.energies = self.cl.cycle_traject_cv(trajectory = self.tr, cv=36)
+        self.energies = self.cl.cycle_traject_cv(trajectory = self.tr, cv=20)
         print(self.energies)
         #print(self.data_raw)
         return json.dumps(self.energies), 201
