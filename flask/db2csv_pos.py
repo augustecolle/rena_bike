db3fdc8fdcdd97b28ff9c1850759407e71613f0e
Auguste <auguste.colle@kuleuven.be>
2017-05-08 12:11:08 +0000

    serial communication updated by editing config.txt on rasp

--- flask/db2csv_pos.py
+++ flask/db2csv_pos.py
@@ -5,15 +5,15 @@ import csv
 db = MySQLdb.connect("localhost", "python_user", "test", "eBike")
 cursor = db.cursor()
 
-cursor.execute("SELECT * FROM eBike.predictions WHERE traject_ID = 26;")
+cursor.execute("SELECT * FROM eBike.measurements")
 a = cursor.fetchall()
-#lats = [x[4] for x in a]
-#longs = [x[5] for x in a]
-#accuracies = [x[7] for x in a]
-#speeds = [x[9] for x in a]
+lats = [x[4] for x in a]
+longs = [x[5] for x in a]
+accuracies = [x[7] for x in a]
+speeds = [x[9] for x in a]
 
-lats = [x[3] for x in a]
-longs = [x[4] for x in a]
+#lats = [x[3] for x in a]
+#longs = [x[4] for x in a]
 
 
 with open('measured.csv', 'wb') as csvfile:
